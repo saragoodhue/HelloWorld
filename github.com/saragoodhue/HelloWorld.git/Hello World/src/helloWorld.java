d40dc0fbfcef7629dec469f0e48f577be8023c78
sara.goodhue <sara.goodhue@MU-C1-12.mullen.pvt.k12.co.us>
2022-05-10 13:17:40 -0600

    Hello World

---
+++ github.com/saragoodhue/HelloWorld.git/Hello World/src/helloWorld.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class helloWorld
+	{
+		public static void main(String[] args)
+			{
+				try (Scanner input = new Scanner(System.in))
+					{
+						int testCases = Integer.parseInt(input.nextLine());
+						for (int testcase = 0; testcase < testCases; testcase++)
+							{
+								System.out.println(input.nextLine());
+							}
+					}
+			}
+	}
